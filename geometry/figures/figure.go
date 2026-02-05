4f4e1e82f7ac54a27481a0b0819fafc1e6e7d47d
Ахтырских Евгений Геннадьевич <aeg85@MacBook-Air-Ahtyrskih.local>
2026-02-05 09:38:06 +0600

    Откатили к началу

--- geometry/figures/figure.go
+++
@@ -1,7 +0,0 @@
-package figures
-
-type FigureParams struct {
-	Width  float64
-	Height float64
-	Radius float64
-}
--- main.go
+++ main.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"study/geometry"
-	"study/geometry/figures"
-
-	"github.com/k0kubun/pp"
-)
-
 func main() {
 
-	params := figures.FigureParams{
-		Width:  1,
-		Height: 2,
-		Radius: 1,
-	}
-
-	figure := figures.NewSquare(params)
-
-	geometryModule := geometry.GeometryModule{
-		Figure: figure,
-	}
-	pp.Println("Площадь фигуры: ", geometryModule.Figure.GetArea())
-	pp.Println("Периметр фигуры: ", geometryModule.Figure.GetPerimetr())
-	pp.Println("Длина окружности: ", geometryModule.Figure.GetCircleLength())
 }
