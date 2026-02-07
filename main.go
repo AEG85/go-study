00dd9200a77c9c028f081d7a0c3f19325090cfba
Ахтырских Евгений Геннадьевич <aeg85@MacBook-Air-Ahtyrskih.local>
2026-02-08 00:01:54 +0600

    Домашка по паники готова

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
+	defer func() {
+		p := recover()
+		if p != nil {
+			fmt.Println("Возникла паника: ", p)
+		}
+	}()
+	// a := 0
+	// b := 1 / a
+	// fmt.Println(b, a)
+
+	// var mapTest map[int]int
+	// mapTest[2] = 123
+
+	// slice := make([]int, 2, 2)
+	// c := slice[4]
+	// fmt.Println(c)
+
+	panic("💥 Это моя собственная паника! Я сам её вызвал!")
 
 }
