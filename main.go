dddb7d808fa5bbe9acc1ad219c7aebe1135f9a70
Ахтырских Евгений Геннадьевич <aeg85@MacBook-Air-Ahtyrskih.local>
2026-02-08 20:16:53 +0600

    Домашка1 по горутинам и аксиомам закрытым каналам готова

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
 func main() {
+	// var chanNill chan int
+	// close(chanNill)
+
+	// close(chanNill)
+
+	// go func() {
+	// 	chanNill <- 2
+	// }()
+	// v := <-chanNill
+	// fmt.Println(v)
+
+	// chanClouse := make(chan int)
+	// close(chanClouse)
+
+	// close(chanClouse)
+
+	// chanClouse <- 1
+
+	// v, ok := <-chanClouse
+	// if ok {
+	// 	fmt.Println("Это не дефолтное значение канала", v)
+	// }
+	// fmt.Println(v)
 
 }
