84d7ff93271753ec0111be5e17b4b1e166a66fa1
Ахтырских Евгений Геннадьевич <aeg85@MacBook-Air-Ahtyrskih.local>
2026-02-08 17:36:18 +0600

    Домашка2 по горутинам и каналам готова

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+)
 
+func main() {
+	chan1 := make(chan int)
+	chan2 := make(chan int)
+	chan3 := make(chan int)
+
+	go RandomInt(chan1)
+	go RandomInt(chan2)
+	go RandomInt(chan3)
+
+	fmt.Println("Пишем из первого канала: ", <-chan1)
+	fmt.Println("Пишем из второго канала: ", <-chan2)
+	fmt.Println("Пишем из третьего канала: ", <-chan3)
+
+}
+
+func RandomInt(ch chan int) {
+	random := rand.Int()
+	ch <- random
 }
