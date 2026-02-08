f30f1669a89f4f8f76304a56941c4d88b01c34d2
Ахтырских Евгений Геннадьевич <aeg85@MacBook-Air-Ahtyrskih.local>
2026-02-08 18:02:28 +0600

    Домашка1 по ананимным функциям готова

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
 
+func main() {
+	go func() {
+		for i := range 3 {
+			fmt.Println("Я 1 горутина мой текст: " + strconv.Itoa(i))
+		}
+	}()
+
+	go func() {
+		for i := range 3 {
+			time.Sleep(1 * time.Second)
+			fmt.Println("Я 2 горутина мой текст: " + strconv.Itoa(i))
+		}
+	}()
+
+	go func() {
+		for i := range 3 {
+			fmt.Println("Я 3 горутина мой текст: " + strconv.Itoa(i))
+		}
+	}()
+	time.Sleep(4 * time.Second)
 }
