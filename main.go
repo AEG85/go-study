cd8fd336a690727facbe2b2a43ac6490d63f561c
Ахтырских Евгений Геннадьевич <aeg85@MacBook-Air-Ahtyrskih.local>
2026-02-08 20:58:46 +0600

    Домашка2 по закрытым горутинам и аксиомам готова

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 func main() {
 
+	ch := make(chan string)
+
+	go TakeInterview(ch)
+
+	for text := range ch {
+		fmt.Println("Человек сообщил следующее: ", text)
+	}
+
+}
+
+func TakeInterview(ch chan string) {
+	timeSlice := []int{300, 400, 500, 600, 700}
+	timeIndex := rand.Intn(len(timeSlice))
+	randLetters := "asdfqwrexvbaserqwetasdf2345kljfaslha"
+	N := rand.Intn(30)
+	for i := range N {
+		if timeIndex+i < len(timeSlice)-1 {
+			timeIndex += i
+		}
+		time.Sleep(time.Duration(timeSlice[timeIndex]) * time.Millisecond)
+
+		wordSlice := make([]byte, 5)
+		for i := range 5 {
+			letterIndex := rand.Intn(len(randLetters) - 1)
+			wordSlice[i] = randLetters[letterIndex]
+		}
+		ch <- string(wordSlice)
+	}
+	close(ch)
 }
