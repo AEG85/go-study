0e34d3c004d065a4e0ba0145fdbbe3a137b785e0
Ахтырских Евгений Геннадьевич <aeg85@MacBook-Air-Ahtyrskih.local>
2026-02-11 16:07:34 +0600

    Домашка1 по горутинам и waitGroup готова

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
 func main() {
+
+	var wg *sync.WaitGroup = &sync.WaitGroup{}
+
+	for i := range 3 {
+		wg.Add(1)
+		go Gardening(wg, i+1)
+	}
+	wg.Wait()
+
+}
+
+func Gardening(wg *sync.WaitGroup, gardenerNumber int) {
+	defer wg.Done()
+	timeIntervals := []int{500, 600, 700, 800, 900, 1000}
+
+	randIndex := rand.Intn(6)
+	randTimeDuration := timeIntervals[randIndex]
+
+	fmt.Println("Я садовник ", gardenerNumber, ", я поливаю! ", randTimeDuration, "мсек")
+
+	time.Sleep(time.Duration(randTimeDuration) * time.Millisecond)
+	fmt.Println("Я садовник ", gardenerNumber, ", я удобряю!", randTimeDuration, "мсек")
 
 }
