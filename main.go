56015eb870043830e0f9a19d22382279aff62c83
Ахтырских Евгений Геннадьевич <aeg85@MacBook-Air-Ahtyrskih.local>
2026-02-12 22:35:09 +0600

    Домашка2 по горотинам мьютексам атомикам готова

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"sync"
+
+	"github.com/k0kubun/pp"
+)
+
+type Fun struct {
+	Name string
+}
+
 func main() {
 
+	wg := &sync.WaitGroup{}
+	mtx := &sync.Mutex{}
+	mailBox := make([]string, 0, 100_000)
+
+	Vasya := Fun{Name: "Vacya"}
+	Petya := Fun{Name: "Petya"}
+
+	wg.Add(2)
+	go Vasya.SendLetter(&mailBox, wg, mtx)
+	go Petya.SendLetter(&mailBox, wg, mtx)
+
+	wg.Wait()
+
+	mtx.Lock()
+	pp.Println(len(mailBox))
+	mtx.Unlock()
+
+}
+
+func (fun *Fun) SendLetter(mailBox *[]string, wg *sync.WaitGroup, mtx *sync.Mutex) {
+	defer wg.Done()
+
+	message := "Я тебя обожаю!"
+	for i := range 1000 {
+		_ = i
+		mtx.Lock()
+		*mailBox = append(*mailBox, message)
+		mtx.Unlock()
+	}
 }
