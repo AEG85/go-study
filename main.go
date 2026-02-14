7a5b3d97ed70ecc8643d73b0e12e809b5e73e1e6
Ахтырских Евгений Геннадьевич <aeg85@192.168.0.10>
2026-02-15 00:08:15 +0600

    Домашка1 по горутинам и rwmutex готова

--- main.go
+++ main.go
@@ -1,5 +1,66 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"sync"
+	"time"
+)
+
 func main() {
 
+	storage := make(map[string]string)
+	mtx := sync.RWMutex{}
+	wg := sync.WaitGroup{}
+
+	start := time.Now()
+
+	for i := range 100_000 {
+		wg.Add(1)
+		keyVal := strconv.Itoa(i)
+		go AddString(&storage, &wg, &mtx, keyVal, keyVal)
+	}
+
+	for i := range 100_000 {
+		wg.Add(1)
+		key := strconv.Itoa(i)
+		go GetString(&storage, &wg, &mtx, key)
+	}
+
+	wg.Wait()
+
+	fmt.Println("Количество эл-ов в мапе: ", len(storage))
+	fmt.Println("Время работы скрипта: ", time.Since(start))
+
+}
+
+func AddString(storage *map[string]string, wg *sync.WaitGroup, mtx *sync.RWMutex, strKey string, strVal string) bool {
+	defer wg.Done()
+
+	mtx.RLock()
+	_, ok := (*storage)[strKey]
+	mtx.RUnlock()
+	if ok {
+		return false
+	}
+	mtx.Lock()
+	(*storage)[strKey] = strVal
+	mtx.Unlock()
+	return true
+}
+
+func GetString(storage *map[string]string, wg *sync.WaitGroup, mtx *sync.RWMutex, strKey string) (string, error) {
+	defer wg.Done()
+
+	mtx.RLock()
+	str, ok := (*storage)[strKey]
+	mtx.RUnlock()
+
+	if !ok {
+		error := errors.New("Нет такой строки")
+		return "", error
+	} else {
+		return str, nil
+	}
 }
