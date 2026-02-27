79b1fffc01cf98dc1ff91798f3f7cb2eaab50c88
Ахтырских Евгений Геннадьевич <aeg85@MacBook-Air-Ahtyrskih.local>
2026-02-27 08:49:10 +0600

    Домашка1 по первому http серверу готова

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand/v2"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", handlerMain)
+	err := http.ListenAndServe(":9091", nil)
+	if err != nil {
+		fmt.Println("Ошибка: ", err.Error())
+	}
+}
+
+func handlerMain(w http.ResponseWriter, r *http.Request) {
+	topic := []string{
+		"Хорошего дня!",
+		"Добрейшего добра!",
+		"Мира вам, миряне!",
+		"Вечер в хату!",
+	}
+	index := rand.IntN(len(topic))
+	randPhrase := topic[index]
+
+	w.Write([]byte(randPhrase))
 }
