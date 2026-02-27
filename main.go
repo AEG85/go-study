d22656bc3508ec712d98586fd58bdc607a7b62fc
Ахтырских Евгений Геннадьевич <aeg85@MacBook-Air-Ahtyrskih.local>
2026-02-27 09:52:52 +0600

    Домашка2 по первому http серверу готова

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/dog", handlerDog)
+	http.HandleFunc("/cat", handlerCat)
+	http.HandleFunc("/cow", handlerCow)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Главная страница!"))
+		if err != nil {
+			fmt.Println("Произошла ошибка: ", err.Error())
+		}
+	})
+
+	err := http.ListenAndServe("localhost:9091", nil)
+	if err != nil {
+		fmt.Println("Произошла ошибка: ", err.Error())
+	}
+}
+
+func handlerDog(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Я собака, я говорю 'ГАВ'"))
+	if err != nil {
+		fmt.Println("Произошла ошибка: ", err.Error())
+	}
+}
+
+func handlerCat(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Я кошка, я говорю 'Мяу'"))
+	if err != nil {
+		fmt.Println("Произошла ошибка: ", err.Error())
+	}
+}
+
+func handlerCow(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Я корова я говорю 'Мууу'"))
+	if err != nil {
+		fmt.Println("Произошла ошибка: ", err.Error())
+	}
 }
