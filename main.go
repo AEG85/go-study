5698344e031444017437f7d38d26402a6c5c0bb7
Ахтырских Евгений Геннадьевич <aeg85@MacBook-Air-Ahtyrskih.local>
2026-02-28 23:20:07 +0600

    Домашка2 по hppt + body готова

--- main.go
+++ main.go
@@ -3,34 +3,83 @@ package main
 import (
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
+	"strconv"
 	"sync"
 )
 
-var storage = []string{}
-var mu = sync.RWMutex{}
+var storage = make(map[int]string, 100)
+var mu = sync.Mutex{}
 
 func main() {
-	http.HandleFunc("/send", mainHandler)
+	http.HandleFunc("/send", addMessageHandler)
+	http.HandleFunc("/delete/", deleteMessageHandler)
 	err := http.ListenAndServe(":9091", nil)
 	if err != nil {
 		fmt.Println("Ошибка сервера: ", err)
 	}
-
 }
 
-func mainHandler(w http.ResponseWriter, r *http.Request) {
+func addMessageHandler(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		fmt.Println("Произошла ошибка при чтении: ", err)
 		return
 	}
 	mu.Lock()
-	storage = append(storage, string(body))
+	randIndex := getRandIndex()
+	storage[randIndex] = string(body)
+	fmt.Println("Добавлено сообщеине:", string(body))
+	printStorage()
 	mu.Unlock()
-	mu.RLock()
-	for _, val := range storage {
-		fmt.Println(val)
+}
+
+func deleteMessageHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Произошла ошибка при чтении: ", err)
+		return
 	}
-	mu.RUnlock()
+
+	mu.Lock()
+	index, err := strconv.Atoi(string(body))
+	if err != nil {
+		fmt.Println("ID элемента должно быть числом! ", string(body))
+		printStorage()
+		mu.Unlock()
+		return
+	}
+	if _, ok := storage[index]; !ok {
+		fmt.Println("Нет элемента с таким ID:", string(body))
+		printStorage()
+		mu.Unlock()
+		return
+	}
+	fmt.Println("Удаляем сообщение:", storage[index])
+	delete(storage, index)
+	printStorage()
+	mu.Unlock()
+}
+
+func getRandIndex() int {
+	for {
+		storageLen := len(storage)
+		if storageLen < 1 {
+			storageLen = 1
+		}
+		storageLen *= 2
+		index := rand.Intn(storageLen)
+		if _, ok := storage[index]; ok {
+			continue
+		}
+		return index
+	}
+
+}
+
+func printStorage() {
+	for key, val := range storage {
+		fmt.Println(key, val)
+	}
 }
