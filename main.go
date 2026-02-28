8dbc8baa2f9b358b89d344eca4a8590208c02ead
Ахтырских Евгений Геннадьевич <aeg85@MacBook-Air-Ahtyrskih.local>
2026-02-28 21:49:36 +0600

    Домашка1 по http запросам с body готовав

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"sync"
+)
 
+var storage = []string{}
+var mu = sync.RWMutex{}
+
+func main() {
+	http.HandleFunc("/send", mainHandler)
+	err := http.ListenAndServe(":9091", nil)
+	if err != nil {
+		fmt.Println("Ошибка сервера: ", err)
+	}
+
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Произошла ошибка при чтении: ", err)
+		return
+	}
+	mu.Lock()
+	storage = append(storage, string(body))
+	mu.Unlock()
+	mu.RLock()
+	for _, val := range storage {
+		fmt.Println(val)
+	}
+	mu.RUnlock()
 }
