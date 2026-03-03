2af93c15e2f9eb779586e66e70d4baec26ade301
Ахтырских Евгений Геннадьевич <aeg85@MacBook-Air-Ahtyrskih.local>
2026-03-03 21:55:27 +0600

    Домашка1 по http qury готова

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", mainHandler)
+	if err := http.ListenAndServe(":9091", nil); err != nil {
+		fmt.Println("Не удалось запустить сервер!")
+		return
+	}
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		if _, err := w.Write([]byte("Не правильный метод запроса")); err != nil {
+			fmt.Println("Не получилось записать ответ")
+			return
+		}
+		return
+	}
+	name := r.URL.Query().Get("name")
+	fmt.Println("Имя: ", name)
+	lastName := r.URL.Query().Get("lastName")
+	fmt.Println("Фамилия: ", lastName)
+	params := r.URL.Query()
+	for k, v := range params {
+		fmt.Printf("Название: %s, Значение: %s \n", k, v[0])
+	}
 }
