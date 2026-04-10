9f8361ab0716ba1d984cba7dc5b4201b819034a0
Ахтырских Евгений Геннадьевич <aeg85@MacBook-Air-Ahtyrskih.local>
2026-04-10 08:22:30 +0600

    Домашка1 по makefile готова

--- main.go
+++ main.go
@@ -1,16 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
 func main() {
-	myAge := os.Getenv("my_age")
-
-	if myAge != "" {
-		fmt.Println("Мой возраст:", myAge)
-	} else {
-		fmt.Println("Возраст не задан!")
-	}
 }
