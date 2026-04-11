2179a1bca5164e2c0ce1b55df3e08567171ac809
Ахтырских Евгений Геннадьевич <aeg85@MacBook-Air-Ahtyrskih.local>
2026-04-11 22:57:12 +0600

    Домашка2 по docker готова

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	counter := 5
+	for i := range counter {
+		fmt.Println("Hello from Docker!: ", i)
+		time.Sleep(time.Second * 1)
+	}
 }
