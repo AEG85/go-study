ebd200f9909f96063115a52d6378a0bf099129ac
Ахтырских Евгений Геннадьевич <aeg85@MacBook-Air-Ahtyrskih.local>
2026-04-14 12:54:51 +0600

    Домашка1-2 по signals готова

--- main.go
+++ main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"syscall"
+	"time"
+)
 
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM)
+	defer cancel()
+	wg := &sync.WaitGroup{}
+
+	countWorkers, err := strconv.Atoi(os.Getenv("WORKERS_COUNT"))
+	if err != nil {
+		fmt.Println("Не корректное число рабочих:", err)
+		return
+	}
+
+	for workerInderx := range countWorkers {
+		wg.Add(1)
+		go work(ctx, wg, workerInderx+1)
+	}
+
+	wg.Wait()
+	fmt.Println("Все рабочие завершили работу")
+}
+
+func work(ctx context.Context, wg *sync.WaitGroup, workerNumber int) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Работник №", workerNumber, "закончил свою работу!")
+			return
+		default:
+			<-time.After(2 * time.Second)
+			fmt.Println("Работник №", workerNumber, "работает...")
+		}
+	}
 }
